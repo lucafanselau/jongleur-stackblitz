b4a651453704850afcb594d58744f475a36e4b24
lucafanselau <59867597+lucafanselau@users.noreply.github.com>
2022-12-14 16:30:40 +0100

    Create basic starter application

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Scroll, orchestrate, helpers, useRegister } from 'jongleur';
+import { Vector3 } from 'three';
+
+const clips = orchestrate(
+  {
+    box: {
+      scale: new Vector3(0, 0, 0),
+      rotation: new Vector3(0, 0, 0),
+    },
+  },
+  {
+    box: {
+      1: {
+        scale: helpers.state(new Vector3(1, 1, 1)),
+        rotation: helpers.state(new Vector3(0, (2 / 3) * Math.PI, 0)),
+      },
+    },
+  },
+  {
+    damping: true,
+    interpolation: 'linear',
+  }
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  const register = useRegister(clips);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Canvas>
+      <Scroll.Controls clips={clips} damping={2}>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <mesh ref={register('box')}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={'hotpink'} />
+        </mesh>
+        <Scroll.Html fixed>
+          <Scroll.At at={0} align={'center'} placement={['start', 'center']}>
+            <h3 style={{ marginTop: 24 }}>This will stay here</h3>
+          </Scroll.At>
+        </Scroll.Html>
+        <Scroll.Html>
+          <Scroll.At at={0.8} align={'center'}>
+            <h1 style={{ textAlign: 'center', width: '80vw' }}>
+              This will scroll with the progress
+            </h1>
+          </Scroll.At>
+        </Scroll.Html>
+      </Scroll.Controls>
+    </Canvas>
+  );
 }
 
-export default App
+export default App;
